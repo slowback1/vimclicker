12a51623d2c90f50ccd6361e4bb3413b6d51faf3
slowback1 <dwobeck@gmail.com>
2023-07-31 20:36:55 -0500

    refactorings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,6 @@
 	import VimButton from '$lib/components/VimButton.svelte';
 	import { addMessage } from '$lib/stores/messageStore';
 
-	let vimClicked = false;
-
 	let onVimClick = () => addMessage("This is Vim!  You don't use a mouse for Vim!");
 </script>
 
