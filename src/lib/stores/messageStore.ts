484b88508998369e5a9b80ae75b6c8db63093baa
slowback1 <dwobeck@gmail.com>
2023-07-31 20:25:37 -0500

    minor formatting changes

--- src/lib/stores/messageStore.ts
+++ src/lib/stores/messageStore.ts
@@ -1,12 +1,13 @@
 import { writable } from "svelte/store";
 
-
 const messageQueue = writable<string[]>([]);
 
-
 export const subscribeToMessages = messageQueue.subscribe;
+
 export const addMessage = (message => messageQueue.update(state => [...state, message]));
+
 export const clearMessages = () => messageQueue.set([]);
+
 export const removeMesage = (index: number) => messageQueue.update((state) => {
     state.splice(index, 1);
     return state;
