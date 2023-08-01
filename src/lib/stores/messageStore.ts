1f7fe1aecd66dbc18d293f6b49602443f26de391
slowback1 <dwobeck@gmail.com>
2023-07-31 20:26:19 -0500

    minor naming change

--- src/lib/stores/messageStore.ts
+++ src/lib/stores/messageStore.ts
@@ -1,14 +1,14 @@
 import { writable } from "svelte/store";
 
-const messageQueue = writable<string[]>([]);
+const messageStore = writable<string[]>([]);
 
-export const subscribeToMessages = messageQueue.subscribe;
+export const subscribeToMessages = messageStore.subscribe;
 
-export const addMessage = (message => messageQueue.update(state => [...state, message]));
+export const addMessage = (message => messageStore.update(state => [...state, message]));
 
-export const clearMessages = () => messageQueue.set([]);
+export const clearMessages = () => messageStore.set([]);
 
-export const removeMesage = (index: number) => messageQueue.update((state) => {
+export const removeMesage = (index: number) => messageStore.update((state) => {
     state.splice(index, 1);
     return state;
 })
